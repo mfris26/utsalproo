ee3e4ff4bd030490037d2a987cbb2699b1cc5d86
mfris26 <farisalifiansyah95@gmail.com>
2025-11-09 16:21:44 +0700

    Create variabel.java
    tugas ulangan

---
+++ variabel.java
@@ -0,0 +1,22 @@
+public class variabel {
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan tipe data yang sesuai
+        String nama = "Muhammad Faris Alifiansyah";
+        String tempatTanggalLahir = "Bogor, 26 Februari 2006";
+        String jenisKelamin = "Laki-laki";
+        String alamatLengkap = "Jl. Rancamaya km.01 Bogor Selatan";
+        String noHp = "08967435467";
+        Double penghasilanPerbulan = 1500000.0;
+
+        System.out.println("Assalamualaikum Wr. Wb.");
+        System.out.println("Izinkan saya memperkenalkan diri:");
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Tempat, Tanggal Lahir: " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Alamat Lengkap: " + alamatLengkap);
+        System.out.println("No. HP: " + noHp);
+        System.out.println("Penghasilan Perbulan: Rp " + penghasilanPerbulan);
+
+    }
+}
